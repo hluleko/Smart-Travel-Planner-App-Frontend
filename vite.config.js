b09d50e382c397a83d3a1246620d143bb14fb5db
hluleko <130248982+hluleko@users.noreply.github.com>
2025-04-14 21:00:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   base: '/', // Change this to the subdirectory where your app will be hosted, or use '/' if it's hosted at the root.
 })
+export default defineConfig({
+  base: '/hluleko/Smart-Travel-App-Frontend/', // same as GitHub repo name
+  plugins: [vue()],
+});
